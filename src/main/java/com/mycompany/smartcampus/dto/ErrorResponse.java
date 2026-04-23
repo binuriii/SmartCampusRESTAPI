5068fe7e25ae45ddc4c614c9284d102275981bcf
binuriii <binuri20048@gmail.com>
2026-04-23 09:41:24 +0530

    Add ErrorResponse DTO for API error handling

---
+++ src/main/java/com/mycompany/smartcampus/dto/ErrorResponse.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smartcampus.dto;
+
+/**
+ *
+ * @author binuripiyathma
+ */
+
+public class ErrorResponse {
+    
+    private int status;
+    private String message;
+
+    public ErrorResponse() {}
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
