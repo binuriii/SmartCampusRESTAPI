eaaf4f59126330f531988c870c17d2f6c18b6d3a
binuriii <binuri20048@gmail.com>
2026-04-23 08:35:27 +0530

    Service setup and root API endpoint

---
+++ src/main/java/MyApplication.java
@@ -0,0 +1,25 @@
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author binuripiyathma
+ */
+
+@ApplicationPath("/api/v1")
+
+public class MyApplication extends Application {
+    
+    
+    
+    
+    
+    
+    
+}
